2270278c3d4bb2f64c4f8b4ada86be7454fc03b1
hyunsoung1217 <hyunsoung13@gmail.com>
2026-03-18 00:25:45 +0900

    homework3-3.php

---
+++ homework3-3.php
@@ -0,0 +1,18 @@
+<?php
+$n = 6;
+$f = [0, 1, 1];
+
+echo "<pre>i   f_i   f_{i+1}/f_i</pre>";
+
+for ($i = 1; $i <= $n; $i++) {
+  
+    if ($i > 2) {
+        $f[$i] = $f[$i-1] + $f[$i-2];
+    }
+
+    $next_f = ($i == 1) ? 1 : $f[$i] + $f[$i-1];
+    $ratio = $next_f / $f[$i];
+
+    echo "$i   $f[$i]   " . round($ratio, 6) . "<br>";
+}
+?>
