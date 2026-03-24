043162613bc14fca70a2e4f590c83f975e29bab0
hyunsoung1217 <hyunsoung13@gmail.com>
2026-03-24 11:54:21 +0900

    homework3-4.php

--- homework3-4.php
+++ homework3-4.php
@@ -1,13 +1,21 @@
 <html>
 <body>
 <head><meta charset="UTF-8"></head>
-밑변<?php echo $_POST["width"]; ?><br>
-높이<?php echo $_POST["height"]; ?><br>
-삼각형<?php echo $_POST["width"]*$_POST["height"]/2;?><br>
-직사각형<?php echo $_POST["width"]*$_POST["height"];?><br>
-원<?php echo $_POST["radius"]**2*pi();?><br>
-직육면체<?php echo $_POST["width"]*$_POST["height"]*$_POST["legth"];?><br>
-원통<?php echo $_POST["radius"]**2*pi()*$_POST["height"];?><br>
-구<?php echo $_POST["radius"]**3*pi()*4/3;?><br>
+<form action="homework3-4.php" method="get">
+밑변(세로): <input type="text" name="width"><br>
+높이: <input type="text" name="height"><br>
+가로: <input type="text" name="legth"><br>
+지름: <input type="text" name="radius"><br>
+<input type="submit">
+</form>
+<?php if (isset($_GET["width"]) && isset($_GET["height"])&& isset($_GET["legth"])&& isset($_GET["radius"])){
+    echo"삼각형 : " . $_GET{"width"}/2*$_GET{"height"}."<br>";
+    echo "직사각형 : " .$_GET{"width"}*$_GET{"height"}."<br>";
+    echo "원 : " . $_GET["radius"]**2*pi()."<br>";
+    echo"직육면체:".$_GET["width"]*$_GET["height"]*$_GET["legth"]."<br>";
+    echo"원통 :".$_GET["radius"]**2*pi()*$_GET["height"]."<br>";
+    echo"구:".$_GET["radius"]*3/4**3*pi()."<br>";
+}
+?>
 </body>
 </html>
